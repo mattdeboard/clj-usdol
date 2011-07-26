7da44770f45fbc820cc224c6ae1246ac518b7bce
Matt DeBoard <matt.deboard@gmail.com>
2011-07-25 17:21:47 -0900

    Added variable wrapper to catch returned data in a var.

--- src/clj_usdol/core.clj
+++ src/clj_usdol/core.clj
@@ -41,8 +41,8 @@
         url (str (str_/join "/" [usdol-url, api-ver, dataset, table]) "?")
         auth (str "Timestamp=" (get-timestamp) "&ApiKey=" token "&Signature="
                   (get-message qs))]
-      (prn (str "URL: " url))
-      (prn (str "Auth: " auth))
       (client/get url
-                  {:headers {"Authorization", auth}})))
-(fetch-data "FORMS" "Agencies")
+                  {:headers {"Authorization" auth
+                             "Accept" "application/json"}})))
+
+(def agency-data (fetch-data "FORMS" "Agencies"))
